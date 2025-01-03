c3c17320401dfebba15379c746d6907f9532776c
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2025-01-04 00:41:59 +0100

    feat: dynamic routing for ngrok

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
-    return {"message": "hello world"}
+async def read_root(request: Request):
+    return {"message": "hello world", "headers": request.headers}
+
+# Catch-all route
+@app.get("/{path:path}")
+async def catch_all(path: str, request: Request):
+    return {"message": f"The path '{path}' was not found on this server.", "headers": request.headers}
