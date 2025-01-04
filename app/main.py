1f3861f3f83576135eca55f0a49a84e340d8cdad
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2025-01-04 15:12:00 +0100

    feat: more informative JSON response from App

--- app/main.py
+++ app/main.py
@@ -1,12 +1,51 @@
 from fastapi import FastAPI, Request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
+
+def headers(request: Request):
+    return {
+        "headers": {
+            key: value
+            for key, value in request.headers.items()
+            if key.startswith("x-") or key == "host"
+        }
+    }
+
+
+def url_scheme(request: Request):
+    if request.url.scheme == "https":
+        return {"scheme": "You are using HTTPS behind the router!"}
+    else:
+        return {"scheme": "You are NOT using HTTPS behind the router!"}
+
+
+def ngrok_url():
+    ngrok_url = os.environ.get("NGROK_DOMAIN")
+    ngrok_url = "https://" + ngrok_url if ngrok_url else None
+    return {"ngrok_url": ngrok_url}
+
+
 @app.get("/")
 async def read_root(request: Request):
-    return {"message": "hello world", "headers": request.headers}
+    return {
+        "message": "hello world",
+        **headers(request),
+        **url_scheme(request),
+        **ngrok_url(),
+    }
+
 
 # Catch-all route
 @app.get("/{path:path}")
 async def catch_all(path: str, request: Request):
-    return {"message": f"The path '{path}' was not found on this server.", "headers": request.headers}
+    return {
+        "message": f"The path '{path}' was not found on this server.",
+        **headers(request),
+        **url_scheme(request),
+        **ngrok_url(),
+    }
